12c58f1f2765236a4d9574c14b537b8e36da7214
dina-fouad <dina_albarghouthi@yahoo.com>
2021-04-01 16:02:45 +0300

    javascript

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("Evaluation")
+var x ;
+x = prompt("Hi, what is your name?") ;
+alert("Hey!"+x+" nice too meet you!") ;
+
+var y = prompt("Please enter your grade")
+if(y > 50) {
+    alert('pass ,Good job') ;
+} else if(y <= 50) {
+    alert('Fail, you need to work hard!') ;
+}else {
+    alert('please enter a number') ;
+}
